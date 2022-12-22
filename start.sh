64f26d56d61de0c86b71d37be66aea36fc2adc5c
Vasiliy Kolmachevskiy <42946191+Waso-python@users.noreply.github.com>
2022-12-22 22:20:00 +0300

    add string in start.sh

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 source /home/user/portal_parsing/venv/bin/activate
 python3 /home/user/portal_parsing/main.py
 python3 /home/user/portal_parsing/json_to_db.py
-
+python3 /home/user/portal_parsing/rss_parse.py
